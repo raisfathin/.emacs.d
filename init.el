dab5a1345574c79b39b5f52dd5f320c9984f64aa
raisfathin <rais.fathin38@gmail.com>
2017-07-23 01:19:44 +1000

    Use linum-relative-mode

--- init.el
+++ init.el
@@ -2,17 +2,3 @@
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (tuareg ido-vertical-mode company-mode use-package solarized-theme projectile moe-theme modern-cpp-font-lock magit google-c-style general flx-ido evil company-ycmd))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
