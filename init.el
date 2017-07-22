ded4ab35b1720d662757bccb5b4029110ecd6501
raisfathin <rais.fathin38@gmail.com>
2017-07-23 00:07:46 +1000

    Move to org-babel-load-file

--- init.el
+++ init.el
@@ -1,26 +1,15 @@
-; (package-initialize)
+(package-initialize)
 
-(add-to-list 'load-path "~/.emacs.d/raisfathin-lisp")
-
-(require 'my-package-config)
-(require 'my-evil-config)
-(require 'my-autocomplete-config)
-(require 'my-appearance-config)
-(require 'my-c++-config)
-(require 'my-project-mgmt-config)
-(require 'my-keybinding-config)
+(org-babel-load-file "~/.emacs.d/configuration.org")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(package-selected-packages
    (quote
-    (projectile magit google-c-style modern-cpp-font-lock solarized-theme moe-theme zenburn-theme company-mode use-package evil company-ycmd))))
+    (tuareg ido-vertical-mode company-mode use-package solarized-theme projectile moe-theme modern-cpp-font-lock magit google-c-style general flx-ido evil company-ycmd))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- raisfathin-lisp/my-project-mgmt-config.el
+++
@@ -1,9 +0,0 @@
-(require 'my-package-config)
-
-(use-package magit
-  :ensure t)
-
-(use-package projectile
-  :ensure t)
-
-(provide 'my-project-mgmt-config)
