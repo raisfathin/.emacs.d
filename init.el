082b8f863b532a9ace9cbc250c669cffcd0221cc
raisfathin <rais.fathin38@gmail.com>
2017-07-30 01:08:58 +1000

    Use smart-mode-line

--- init.el
+++ init.el
@@ -7,9 +7,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
  '(package-selected-packages
    (quote
-    (exec-path-from-shell linum-relative nlinum-relative nlinum tuareg ido-vertical-mode company-mode use-package solarized-theme projectile moe-theme modern-cpp-font-lock magit google-c-style general flx-ido evil company-ycmd))))
+    (smart-mode-line-powerline-theme smart-mode-line nyan-mode weechat exec-path-from-shell linum-relative nlinum-relative nlinum tuareg ido-vertical-mode company-mode use-package solarized-theme projectile moe-theme modern-cpp-font-lock magit google-c-style general flx-ido evil company-ycmd))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
