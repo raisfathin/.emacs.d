199eaa9798d3ee0f05b39732ea356f2f762fe756
raisfathin <rais.fathin38@gmail.com>
2018-06-03 14:54:28 +0800

    Add latest update

--- init.el
+++ init.el
@@ -9,13 +9,16 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
  '(package-selected-packages
    (quote
-    (web-mode smart-mode-line-powerline-theme smart-mode-line nyan-mode weechat exec-path-from-shell linum-relative nlinum-relative nlinum tuareg ido-vertical-mode company-mode use-package solarized-theme projectile moe-theme modern-cpp-font-lock magit google-c-style general flx-ido evil company-ycmd))))
+    (rust-mode nord-theme nord-emacs rainbow-delimiters rjsx-mode flatui-dark-theme haskell-mode utop evil-vimish-fold vimish-fold vue-mode ace-window tide htmlize emacs-htmlize afternoon-theme material-theme material d-mode eshell-git-prompt emms dionysos simple-mpc ox-reveal elm-mode web-mode smart-mode-line-powerline-theme smart-mode-line nyan-mode weechat exec-path-from-shell linum-relative nlinum-relative nlinum tuareg ido-vertical-mode company-mode use-package solarized-theme projectile moe-theme modern-cpp-font-lock magit google-c-style general flx-ido evil company-ycmd))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
