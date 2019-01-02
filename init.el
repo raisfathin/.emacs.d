89a835d45564c88f8db550b7602c54e809f54bbd
raisfathin <rais.fathin38@gmail.com>
2019-01-02 10:39:47 +0700

    Init el

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
  '(package-selected-packages
    (quote
-    (parinfer evil-surround evil-leader slime org-bullets rust-mode nord-theme nord-emacs rainbow-delimiters rjsx-mode flatui-dark-theme haskell-mode utop evil-vimish-fold vimish-fold vue-mode ace-window tide htmlize emacs-htmlize afternoon-theme material-theme material d-mode eshell-git-prompt emms dionysos simple-mpc ox-reveal elm-mode web-mode smart-mode-line-powerline-theme smart-mode-line nyan-mode weechat exec-path-from-shell linum-relative nlinum-relative nlinum tuareg ido-vertical-mode company-mode use-package solarized-theme projectile moe-theme modern-cpp-font-lock magit google-c-style general flx-ido evil company-ycmd))))
+    (fireplace racket-mode zenburn-theme parinfer evil-surround evil-leader slime org-bullets rust-mode nord-theme nord-emacs rainbow-delimiters rjsx-mode flatui-dark-theme haskell-mode utop evil-vimish-fold vimish-fold vue-mode ace-window tide htmlize emacs-htmlize afternoon-theme material-theme material d-mode eshell-git-prompt emms dionysos simple-mpc ox-reveal elm-mode web-mode smart-mode-line-powerline-theme smart-mode-line nyan-mode weechat exec-path-from-shell linum-relative nlinum-relative nlinum tuareg ido-vertical-mode company-mode use-package solarized-theme projectile moe-theme modern-cpp-font-lock magit google-c-style general flx-ido evil company-ycmd))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
